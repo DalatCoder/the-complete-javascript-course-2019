bc45a598ce512d491303669629a20885e57f5620
DalatCoder <hieuntctk42@gmail.com>
2019-04-04 23:06:40 +0700

    Non-object, primary types

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var _3years = 3;
+var $4year = 4;
+var johnMark = 'John and Mark';
