3a388236329a5132f4c0fc56bd2525ea49696607
DalatCoder <hieuntctk42@gmail.com>
2019-04-05 22:59:23 +0700

    Operator precedence

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -55,7 +55,7 @@ console.log(firstName + ' ' + lastName);
  * Basic operators
  */
 
-
+/*
 var year, yearJohn, yearMark;
 now = 2018;
 ageJohn = 28;
@@ -82,6 +82,49 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
 console.log(typeof x);
+*/
+
+/**
+ * Operator precedence
+ */
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6
+console.log(x, y);
+
+
+// More operators
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x = x + 1;
+x += 1;
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+
+
+
+
+
 
 
 
