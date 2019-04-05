96a94b584eac9b91a0aa4d6087f4f0844db4ae3e
DalatCoder <hieuntctk42@gmail.com>
2019-04-05 22:29:52 +0700

    Comment and type coercion

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,3 +1,8 @@
+
+/**
+ * Variables and date types
+ */
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -13,6 +18,38 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+// Variable naming rules
 var _3years = 3;
 var $4year = 4;
 var johnMark = 'John and Mark';
+*/
+
+/**
+ * Variable mutation and type coercion
+ */
+
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old '
++ job + '. Is he married? ' + isMarried) ;
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + 
+job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+
+
+
+
