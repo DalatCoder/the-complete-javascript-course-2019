7c03cf574fc2c01003f738527450a212b346341f
DalatCoder <hieuntctk42@gmail.com>
2019-04-10 22:32:24 +0700

    Functions

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -243,8 +243,31 @@ if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
 	console.log('There is a draw');
 }
 
+/**
+ * Functions
+ */
+function calculateAge(birthYear) {
+	return 2018 - birthYear;
+}
 
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
 
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+	var age = calculateAge(year);
+	var retirement = 65 - age;
+	if (retirement > 0) {
+		console.log(firstName + ' retires in ' + retirement + ' years.');
+	} else {
+		console.log(firstName + ' is already retired.');
+	}
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(2000, 'Hieu');
 
 
 
