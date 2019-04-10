966b4e4b82b42d9f7da93ac32c00f8f1d48cb8d2
DalatCoder <hieuntctk42@gmail.com>
2019-04-10 23:03:03 +0700

    Array

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -292,9 +292,37 @@ yearsUntilRetirement(2000, 'Hieu');
 
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
-console.log(whatDoYouDo('driver', 'Mark'));
+console.log(whatDoYouDo('driver', 'Mark	'));
 
+/**
+ * Arrays
+ */
 
+ // Initialize new array
+ var names = ['John', 'Mike', 'Jane'];
+ var years = new Array(1998, 1969, 1948);
+
+ console.log(names[2]);
+ console.log(names.length);
+
+ // Mutate array data
+ names[1] = 'Ben';
+ names[names.length] = 'Mary';
+ console.log(names);
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
 
 
 
