09058cf9fcb120e17cedc110e455a8c6c0e51a0f
DalatCoder <hieuntctk42@gmail.com>
2019-04-10 22:45:59 +0700

    Functions Statements and Expressions

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -269,6 +269,30 @@ function yearsUntilRetirement(year, firstName) {
 yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(2000, 'Hieu');
 
+/**
+ * Function Statements and Expressions
+ */
+
+ // Function declaration
+
+
+ // Function expression
+ var whatDoYouDo = function(job, firstName) {
+	 switch (job) {
+		 case 'teacher':
+			return firstName + ' teaches kids how to code';
+		 case 'driver':
+			return firstName + ' drives a cab in Lisbon.';
+		 case 'designer':
+			return firstName + ' designs beautiful websites';
+		 default:
+		 return firstName + ' does something else';
+	 }
+ }
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('driver', 'Mark'));
 
 
 
