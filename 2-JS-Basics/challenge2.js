58c31e8100c5182364157a634851752428b44ae6
DalatCoder <hieuntctk42@gmail.com>
2019-04-11 12:14:59 +0700

    Complete coding challenge 2

---
+++ 2-JS-Basics/challenge2.js
@@ -0,0 +1,12 @@
+
+var scoreJohn = (89 + 120 + 103) / 3;
+var scoreMike = (116 + 94 + 123) / 3;
+
+if (scoreJohn > scoreMike) {
+  console.log('John\'s team wins the match');
+} else if (scoreMike > scoreJohn) {
+  console.log('Mike\'s team wins the match.');
+} else {
+  console.log('It\'s a draw match.');
+}
+
