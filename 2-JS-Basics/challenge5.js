3feb8352274524a8881f057184cfadf3ab7d6b9d
DalatCoder <hieuntctk42@gmail.com>
2019-04-11 23:12:44 +0700

    Add extra test

--- 2-JS-Basics/challenge5.js
+++ 2-JS-Basics/challenge5.js
@@ -26,3 +26,28 @@ console.log(johnHoliday.tipCalc());
 console.log(johnHoliday.bills);
 console.log(johnHoliday.tips);
 
+var markHoliday = {
+  bills: [77, 375, 110, 45],
+  tips: [],
+  paids: [],
+  tipCalc: function() {
+    for (var i = 0; i < this.bills.length; i++) {
+      if (this.bills[i] < 100) {
+        this.tips.push(.2);
+      } else if (this.bills[i] < 300) {
+        this.tips.push(.15);
+      } else {
+        this.tips.push(.25);
+      }
+
+      this.paids.push(this.bills[i] + this.bills[i] * this.tips[i]);
+    }
+    return this.paids;
+  }
+}
+
+console.log(markHoliday.tipCalc());
+console.log(markHoliday.bills);
+console.log(markHoliday.tips);
+
+
