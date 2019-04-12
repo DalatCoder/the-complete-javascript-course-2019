7544326c5f37a465793e8700aa5ca0573182fb71
DalatCoder <hieuntctk42@gmail.com>
2019-04-12 22:30:11 +0700

    Closure

--- 3-How-JS-Works/scoping.js
+++ 3-How-JS-Works/scoping.js
@@ -14,7 +14,24 @@
    function second() {
      var c = 'Hey';
      console.log(a + b + c);
+     third();
    }
  }
 
- 
+ // Example to show the difference between execution stack and scope chain
+
+ function third() {
+   var d = 'John';
+   // console.log(c);
+   console.log(a+d);
+ }
+
+// Closure
+function multipler(n) {
+  return number => number * n;
+}
+var twice = multipler(2);
+console.log(twice(5));
+
+
+
