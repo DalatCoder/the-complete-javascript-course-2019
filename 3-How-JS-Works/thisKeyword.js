66dba29dd7ca3c355ea15241c664201dc976f5cc
DalatCoder <hieuntctk42@gmail.com>
2019-04-12 22:40:46 +0700

    John object

--- 3-How-JS-Works/thisKeyword.js
+++ 3-How-JS-Works/thisKeyword.js
@@ -6,3 +6,15 @@ function calculateAge(uear) {
   console.log(2016 - year);
   console.log(this);
 }
+
+var john = {
+  name: 'John',
+  yearOfBirth: 1985,
+  calculateAge: function() {
+    console.log(this);
+  }
+}
+
+john.calculateAge();
+
+
