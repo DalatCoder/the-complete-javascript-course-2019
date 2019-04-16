2f979a924231763a9d51e8debf82ab5b69a84fbf
DalatCoder <hieuntctk42@gmail.com>
2019-04-16 22:08:59 +0700

    Object.create

--- 5-Objects-and-Functions/script.js
+++ 5-Objects-and-Functions/script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/**
 var john = {
   name: 'John',
   yearOfBirth: 1990,
@@ -29,6 +29,27 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+// Object.create
+var personProto = {
+  calculateAge : function() {
+    console.log(2016 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+  name : { value: 'Jane' },
+  yearOfBirth : { value: 1969 },
+  job: { value: 'designer' }
+});
+
+
 
   
 
