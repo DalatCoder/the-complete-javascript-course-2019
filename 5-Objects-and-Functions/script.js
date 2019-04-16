8a2a7cf266b639554471d7cfb2f04482ad71ae4b
DalatCoder <hieuntctk42@gmail.com>
2019-04-16 12:44:39 +0700

    Creating Objects Function Constructors

---
+++ 5-Objects-and-Functions/script.js
@@ -0,0 +1,36 @@
+// Function constructor
+
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2019 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+
+  
+
+
+
