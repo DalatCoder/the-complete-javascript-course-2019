02dd7cd0f035549082b42d49b55c6d2775653a81
DalatCoder <hieuntctk42@gmail.com>
2019-04-17 21:40:55 +0700

    Primitives and Objects

--- 5-Objects-and-Functions/script.js
+++ 5-Objects-and-Functions/script.js
@@ -32,6 +32,7 @@ console.log(mark.lastName);
 */
 
 // Object.create
+/**
 var personProto = {
   calculateAge : function() {
     console.log(2016 - this.yearOfBirth);
@@ -48,6 +49,65 @@ var jane = Object.create(personProto, {
   yearOfBirth : { value: 1969 },
   job: { value: 'designer' }
 });
+*/
+
+
+// Primitive and objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+var obj2 = obj1;
+obj1.age = 38;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+};
+function change(a, b) {
+  a = 38;
+  b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
