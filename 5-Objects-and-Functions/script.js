ae4da9408a80076e4f09bfef2bbc34629fa07e97
DalatCoder <hieuntctk42@gmail.com>
2019-04-17 22:42:01 +0700

    Closure

--- 5-Objects-and-Functions/script.js
+++ 5-Objects-and-Functions/script.js
@@ -178,7 +178,17 @@ game();
   console.log(a + ' + ' + b + ' = ' + sum);
 })(1, 2);
 
+// Closure
+function retirement(retirementAge) {
+  var a = ' years left until retirement';
+  return function(yearOfBirth) {
+    var age = 2019 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+}
 
+var retirementUS = retirement(66);
+retirementUS(2000);
 
 
 
