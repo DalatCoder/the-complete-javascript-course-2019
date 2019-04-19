fbdcce04aec01cb18327c7274d3a5b977f4e7c57
DalatCoder <hieuntctk42@gmail.com>
2019-04-19 08:57:14 +0700

    Add 3 questions for solution

--- 5-Objects-and-Functions/codingChallenge.js
+++ 5-Objects-and-Functions/codingChallenge.js
@@ -6,4 +6,16 @@ function Question(question, answers, correct) {
   this.correct = correct;
 }
 
+var q1 = new Question('Is JavaScript the cooleast programming language in the world!',
+                      ['Yes', 'No'],
+                      0);
+
+var q2 = new Question('What is the name of this course\'s teacher?',
+                      ['John', 'Micheal', 'Jonas'],
+                      2);
+
+
+var q3 = new Question('What does best describe coding?',
+                      ['Boring', 'Hard', 'Fun', 'Tedious'],
+                      2);
 
