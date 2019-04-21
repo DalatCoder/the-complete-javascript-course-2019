c17a4a779acd2b6ec1c84ac37bb6eccf35b1291b
DalatCoder <hieuntctk42@gmail.com>
2019-04-21 23:13:54 +0700

    Hoisting

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,31 +1,10 @@
-/**
- * Lecture: Hoisting
- */
 
+CalculateAge(1990);
 
- // Functions
- calculateAge(1965);
-
- function calculateAge(year) {
-   console.log(2016 - year);
- }
-
- var retirement = function(year) {
-   console.log(65 - (2016 - year));
- }
-
-// variables
-console.log(age);
-var age = 23;
-console.log(age);
-
-function foo() {
-  console.log(age);
-  var age = 65;
-  console.log(age);
+function CalculateAge(year) {
+  console.log(2016 - year);
 }
-foo();
-console.log(age);
 
-
-
+var retirement = function(year) {
+  console.log(65 - (2016 - year));
+}
