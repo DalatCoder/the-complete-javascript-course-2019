6e3904a63723d250fa73aeec4c14e04d4cba4bba
DalatCoder <hieuntctk42@gmail.com>
2019-04-21 23:14:53 +0700

    Hoisting

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -8,3 +8,14 @@ function CalculateAge(year) {
 var retirement = function(year) {
   console.log(65 - (2016 - year));
 }
+
+console.log(age);
+var age = 23;
+
+function foo() {
+  console.log(age);
+  var age = 65;
+  console.log(age);
+}
+foo();
+console.log(age);
