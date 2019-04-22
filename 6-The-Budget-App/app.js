2198877a5770a8cc8496a925e181e8b23db8dddd
DalatCoder <hieuntctk42@gmail.com>
2019-04-22 15:52:54 +0700

    Add keypress event to do things when user press Enter key to controller module

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -15,7 +15,7 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  document.querySelector('.btn__add').addEventListener('click', function() {
+  document.querySelector('.add__btn').addEventListener('click', function() {
     // 1. Get the field input data
 
     // 2. Add the item to the budget controller
@@ -25,6 +25,14 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
+
+  });
+
+  document.addEventListener('keypress', function(event) {
+    
+    if (event.keyCode === 13) {
+      
+    }
 
   });
 
