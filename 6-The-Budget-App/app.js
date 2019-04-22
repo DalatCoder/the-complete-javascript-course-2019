2fd5c6382ee168da2d6ad1d24d1928227fb57252
DalatCoder <hieuntctk42@gmail.com>
2019-04-22 15:39:28 +0700

    Add eventListener to controller module

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -1,36 +1,36 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  
-  var x = 23;
 
-  var add = function(a) {
-    return x + a;
-  }
 
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
   // Add code later
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  document.querySelector('.btn__add').addEventListener('click', function() {
+    // 1. Get the field input data
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
-  }
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+  });
 
 })(budgetController, UIController);
+
+
 
 
 
