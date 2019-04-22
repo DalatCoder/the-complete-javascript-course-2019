0d2a44da441979fb1311d6fb792530746f730ac1
DalatCoder <hieuntctk42@gmail.com>
2019-04-22 23:01:52 +0700

    Add DOMstrings object to control class name

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -8,12 +8,18 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value'
+  }
+
   return {
     getInput: function() {
       return {
-        type: document.querySelector('.add__type').value,
-        description: document.querySelector('.add__description').value,
-        value: document.querySelector('.add__value').value
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       };
     }
   };
