a7cc98b04b471c062ebe1535ba6e38487eaa5f17
DalatCoder <hieuntctk42@gmail.com>
2019-04-24 23:25:04 +0700

    Create random number

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -5,3 +5,17 @@ activePlayer = 0;
 scores = [0, 0];
 roundScore = 0;
 
+// Hide the dice by default
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+  // 1. Create random number  
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display
+
+
+  // 3. Update current score
+
+});
