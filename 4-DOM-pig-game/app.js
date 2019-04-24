a91670e9671f0062121acc03ab4e591959e26291
DalatCoder <hieuntctk42@gmail.com>
2019-04-24 23:52:10 +0700

    Fix bugs

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -71,5 +71,10 @@ function init() {
   document.getElementById('current-0').textContent = 0;
   document.getElementById('current-1').textContent = 0;
   
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.add('active');
 }
 
