372da0d5ad259b161d5d96f6281384ca90014491
DalatCoder <hieuntctk42@gmail.com>
2019-04-25 22:26:42 +0700

    Add Expense constructor

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -1,7 +1,11 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
 
 })();
 
