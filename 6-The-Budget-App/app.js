e113cdd7f27cb5477116b6aade549ef759c33d65
DalatCoder <hieuntctk42@gmail.com>
2019-04-25 23:15:31 +0700

    Html string for expense

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -77,7 +77,9 @@ var UIController = (function() {
     addListItem: function(obj, type) {
       var html;
       // Create HTML string with placeholder text
-      html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+
+      html = '<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 
       // Replace the placeholder text with some actual data
 
