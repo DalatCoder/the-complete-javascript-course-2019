87c62fb5b00a7a688f5fc9f1a709f07a78a5c242
DalatCoder <hieuntctk42@gmail.com>
2019-04-25 22:27:26 +0700

    Add Income constructor:

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -7,6 +7,12 @@ var budgetController = (function() {
     this.value = value;
   }
 
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
 })();
 
 // UI CONTROLLER
