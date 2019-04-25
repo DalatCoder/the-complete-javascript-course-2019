5e7135087b9f8eb7f7347641713cbc9197dd13f6
DalatCoder <hieuntctk42@gmail.com>
2019-04-25 23:13:03 +0700

    Create html string

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -73,6 +73,18 @@ var UIController = (function() {
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+
+    addListItem: function(obj, type) {
+      var html;
+      // Create HTML string with placeholder text
+      html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+
+      // Replace the placeholder text with some actual data
+
+      // Insert the HTML into the DOM
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
