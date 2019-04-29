bf0bb05202c2238dbfd39e35dd4ce0d14d976633
DalatCoder <hieuntctk42@gmail.com>
2019-04-29 22:10:52 +0700

    Delete item from the UI and update budget on the UI

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -162,6 +162,15 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+
+    deleteListItem: function(selectorID) {
+      
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
+
+    },
+
+
     clearFields: function() {
       var fields, fieldsArr;
 
@@ -273,8 +282,10 @@ var controller = (function(budgetCtrl, UICtrl) {
       budgetCtrl.deleteItem(type, ID);
 
       // 2. Delete the item from the UI
+      UICtrl.deleteListItem(itemID);
 
       // 3. Update and show the new budget
+
 
     }
 
