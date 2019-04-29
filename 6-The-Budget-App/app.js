1395ee044538b80c68d5b1b278ee0110548920d5
DalatCoder <hieuntctk42@gmail.com>
2019-04-29 12:37:22 +0700

    Add displayBudget function to init method to set all value to 0

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -104,8 +104,12 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
-  }
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
+  };
 
   return {
     getInput: function() {
@@ -157,6 +161,20 @@ var UIController = (function() {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function(obj) {
+    
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+      
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -189,7 +207,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
 
   };
 
@@ -218,6 +236,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
+      
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
+
       setupEventListener();
     }
   };
