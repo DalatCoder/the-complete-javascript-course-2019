e043358e6cfaa57b5c3419db8627c649f2de649d
DalatCoder <hieuntctk42@gmail.com>
2019-04-29 11:12:45 +0700

    Parse input value from string to float number

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -49,7 +49,7 @@ var budgetController = (function() {
       return newItem;
     },
     testing: function() {
-      console.log(data.allItems);
+      console.log(data);
     }
   };
 
@@ -72,7 +72,7 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
