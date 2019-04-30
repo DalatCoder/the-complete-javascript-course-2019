8b8d37a6151e341ba3f99d0f5c2bc546276dacf3
DalatCoder <hieuntctk42@gmail.com>
2019-04-30 11:56:23 +0700

    Update percentages from the data structure

--- 6-The-Budget-App/app.js
+++ 6-The-Budget-App/app.js
@@ -272,11 +272,14 @@ var controller = (function(budgetCtrl, UICtrl) {
   var updatePercentages = function() {
 
     // 1. Calculate percentages
+    budgetCtrl.calculatePercentages();
 
     // 2. Read percentages from the budget controller
+    var percentages = budgetCtrl.getPercentages();
 
     // 3. Update the UI with the new percentages
-
+    console.log(percentages);
+    
   };
 
 
