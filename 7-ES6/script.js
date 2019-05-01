c97ed08f7055fbc49d65ae4c4153051824655a2c
DalatCoder <hieuntctk42@gmail.com>
2019-05-01 21:55:01 +0700

    Declare variable with const and let

---
+++ 7-ES6/script.js
@@ -0,0 +1,19 @@
+
+/**
+ * Lecture 1: Let and const
+ */
+
+// ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+/*
+const name6 = 'Jane Smith';
+let age6 = 23;
+name6 = 'Jane Miller'; // error
+console.log(name6); 
+*/
+
