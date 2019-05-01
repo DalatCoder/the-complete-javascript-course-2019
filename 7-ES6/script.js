a08be9d18f5116a849672cbc1c69e8eff278a948
DalatCoder <hieuntctk42@gmail.com>
2019-05-01 22:02:41 +0700

    Block scope with let and const

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -3,17 +3,50 @@
  * Lecture 1: Let and const
  */
 
+ /*
 // ES5
 var name5 = 'Jane Smith';
 var age5 = 23;
 name5 = 'Jane Miller';
 console.log(name5);
 
+
 // ES6
-/*
 const name6 = 'Jane Smith';
 let age6 = 23;
 name6 = 'Jane Miller'; // error
 console.log(name6); 
 */
+
+// ES5
+function driverLisence5(passedTest) {
+
+  if (passedTest) {
+    var firstName = 'John';
+    var yearOfBirth = 1990;
+
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+  }
+
+}
+driverLisence5(true);
+
+// ES6
+function driverLisence6(passedTest) {
+
+  if (passedTest) {
+    let firstName = 'John';
+    const yearOfBirth = 1990;
+
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car');
+  }
+
+}
+driverLisence6(true);
+
+
+
+
+
+
 
