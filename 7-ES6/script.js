bb39466e11df762c9ed9700eaed42b0cc7f6b06f
DalatCoder <hieuntctk42@gmail.com>
2019-05-02 08:13:33 +0700

    Some new string methods

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -85,5 +85,18 @@ console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + year
 // ES6
 console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
 
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));
+
+console.log(n.endsWith('Sm'));
+
+console.log(n.includes('oh'));
+
+console.log(`${firstName} `.repeat(5));
+
+
+
+
+
 
 
