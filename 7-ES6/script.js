aff50d49b9741c754602b47258c83fbfcbdbd48f
DalatCoder <hieuntctk42@gmail.com>
2019-05-02 08:36:33 +0700

    This pointer in ES5 and a trick

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -123,4 +123,18 @@ ages6 = years.map((el, index) => {
 console.log(ages6);
 
 
+// ES5
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    var self = this;
+    document.querySelector('.green').addEventListener('click', function() {
+      var str = 'This is box number: ' + self.position + ' and it is ' + self.color;
+      alert(str);
+    });
+  }
+};
+box5.clickMe();
+
 
