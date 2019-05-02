279d58437fac392d4a67304bd61645a34f4a84f0
DalatCoder <hieuntctk42@gmail.com>
2019-05-02 08:26:23 +0700

    Arrow function with more than one arguments and instructions

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -108,12 +108,19 @@ var ages5 = years.map(function(current) {
 console.log(ages5);
 
 // ES6
-const ages6 = years.map(  el => 2019 - el  );
+let ages6 = years.map(  el => 2019 - el  );
 console.log(ages6);
 
 
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
 
-
+ages6 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${index + 1}: ${age}`;
+});
+console.log(ages6);
 
 
 
