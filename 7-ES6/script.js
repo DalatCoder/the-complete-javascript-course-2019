2bc8226cb42e26b8d9e1b496f957b092edfbf8b0
DalatCoder <hieuntctk42@gmail.com>
2019-05-02 20:29:15 +0700

    Default parameters

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -350,4 +350,41 @@ function isFullAge6(limit, ...years) {
 isFullAge6(1990, 1999, 1965, 2016, ...[1990, 2010]);
 
 
+/**
+ * Lecture: Default parameters
+ */
+
+ // ES5
+ /*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+
+  lastName === undefined ? lastName = 'Smith' : lastName;
+  nationality === undefined ? nationality = 'american' : nationality;
+
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
+}
+*/
+
+// ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'American') {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.nationality = nationality;
+  this.yearOfBirth = yearOfBirth;
+}
+
+var john1 = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
+
+
+
+
+
+
+
+
+
 
