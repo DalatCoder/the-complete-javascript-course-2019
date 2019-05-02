1d380fe9ab453b0e3042f652552be84f98feace4
DalatCoder <hieuntctk42@gmail.com>
2019-05-02 22:09:43 +0700

    Classes

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -409,8 +409,71 @@ for (let [key, value] of question.entries()) {
   }
 }
 
-const ans = parseInt(prompt('Write the correct answer: '));
-console.log(question.get(ans === question.get('correct')));
+// const ans = parseInt(prompt('Write the correct answer: '));
+// console.log(question.get(ans === question.get('correct')));
+
+/**
+ * Lecture: Classes
+ */
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'Teacher');
+john5.calculateAge();
+
+// ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calculateAge() {
+    const age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log('Hey there!');
+  }
+
+}
+
+const john6 = new Person6('John', 1990, 'Teacher');
+john6.calculateAge();
+
+Person6.greeting();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
