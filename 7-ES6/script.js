9b8f774839f2bbeba8a360f33ebf8545e9acd21a
DalatCoder <hieuntctk42@gmail.com>
2019-05-02 11:13:06 +0700

    Rest parameter

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -306,8 +306,28 @@ const all = [h, ...boxes];
 Array.from(all).forEach(el => el.style.color = 'purple');
 
 
+/**
+ * Lecture: Rest parameters
+ */
 
+ // ES5
+function isFullAge5() {
+  // console.log(arguments);
+  var argsArr = Array.prototype.slice.call(arguments);
+  
+  argsArr.forEach(function(el) {
+    console.log((2016 - el) >= 18);
+  });
+}
 
+// isFullAge5(1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+// ES6
+function isFullAge6(...years) {
+  years.forEach(el => console.log((2016 - el) >= 18));
+}
+isFullAge6(1990, 1999, 1965, 2016, ...[1990, 2010]);
 
 
 
