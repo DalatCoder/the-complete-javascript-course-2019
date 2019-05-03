3feba74ca0d64263276d56ffe032d75933d29b09
DalatCoder <hieuntctk42@gmail.com>
2019-05-03 21:41:46 +0700

    Add class Park extends from class Element

--- 7-ES6/codingChallenge.js
+++ 7-ES6/codingChallenge.js
@@ -22,11 +22,14 @@ class Element {
     this.name = name;
     this.buildYear = buildYear;
   }
+}
 
-
-
-
-  
+class Park extends Element {
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear);
+    this.area = area;
+    this.numTrees = numTrees;
+  }
 }
 
 
