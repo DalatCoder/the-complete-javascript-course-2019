c5ad0850e4fb1284ac049c64ae7f38158245a782
DalatCoder <hieuntctk42@gmail.com>
2019-05-03 21:37:35 +0700

    Add superClass

---
+++ 7-ES6/codingChallenge.js
@@ -0,0 +1,32 @@
+/**
+ * Suppose that you're working in a small town administration, and you're in charge of two town elements:
+ * 1. Parks
+ * 2. Streets
+ * 
+ * It's a very small town, so right now there are only 3 parks and 4 streets have a name and a build year.
+ * 
+ * At an end-of-year meeting, your boss wantsa final report with the following:
+ * 1. Tree density of each park in the twon (formula: number of trees/park area)
+ * 2. Average age of each town's park (formula: sum of all ages/number of parks)
+ * 3. The name of the park that has more than 1000 trees
+ * 4. Total and average length of the town's streets
+ * 5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+ * 
+ * All the report data should be printed to the console.
+ * 
+ * HINT: Use some of the ES6 feature: classes, subclasses, template strings, default parameters,maps, arrow functions, destructuring, etc.
+ */
+
+class Element {
+  constructor(name, buildYear) {
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+
+
+
+
+  
+}
+
+
