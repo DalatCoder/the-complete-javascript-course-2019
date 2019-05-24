6cc1c1f5ad16ac604d927a8d51785ca356f7b961
DalatCoder <hieuntctk42@gmail.com>
2019-05-24 09:31:23 +0700

    Rolling the dice

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -8,3 +8,27 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    // 1. Random number
+    const dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    document.querySelector('.dice').style.display = 'block';
+    document.querySelector('.dice').src = `dice-${dice}.png`;
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+});
