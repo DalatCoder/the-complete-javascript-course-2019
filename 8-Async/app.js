ba7abcd20a7925534667ff6d60ce77b8d34c987d
DalatCoder <hieuntctk42@gmail.com>
2019-05-27 18:31:09 +0700

    Callback hell

--- 8-Async/app.js
+++ 8-Async/app.js
@@ -1,1 +1,26 @@
 // Callback hell
+function getRecipe() {
+
+  setTimeout(() => {
+    const IDs = [111, 222, 333, 444];
+    console.log(IDs);
+
+    setTimeout(id => {
+      const recipe = {
+        title: 'Fresh tomoto pasta',
+        publisher: 'Trong Hieu'
+      };
+      console.log(`${id}: ${recipe.title}.`);
+
+      setTimeout(pub => {
+        const recipe2 = {
+          title: 'Italian Pizza',
+          publisher: pub
+        }
+        console.log(`${recipe2.title} from ${recipe2.publisher}.`);
+      }, 2000, recipe.publisher);
+    }, 2000, IDs[2]);
+  }, 2000);
+
+};
+getRecipe();
