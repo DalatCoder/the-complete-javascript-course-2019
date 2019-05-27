e0ebb09fc4c3839904ba5c369830007c8a130025
DalatCoder <hieuntctk42@gmail.com>
2019-05-27 18:58:37 +0700

    Promises

--- 8-Async/app.js
+++ 8-Async/app.js
@@ -1,26 +1,75 @@
-// Callback hell
-function getRecipe() {
+// // Callback hell
+// function getRecipe() {
 
+//   setTimeout(() => {
+//     const IDs = [111, 222, 333, 444];
+//     console.log(IDs);
+
+//     setTimeout(id => {
+//       const recipe = {
+//         title: 'Fresh tomoto pasta',
+//         publisher: 'Trong Hieu'
+//       };
+//       console.log(`${id}: ${recipe.title}.`);
+
+//       setTimeout(pub => {
+//         const recipe2 = {
+//           title: 'Italian Pizza',
+//           publisher: pub
+//         }
+//         console.log(`${recipe2.title} from ${recipe2.publisher}.`);
+//       }, 2000, recipe.publisher);
+//     }, 2000, IDs[2]);
+//   }, 2000);
+
+// };
+// getRecipe();
+
+// Promises
+// Produce
+const getIDs = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const IDs = [111, 222, 333, 444];
-    console.log(IDs);
+    resolve([111, 222, 333, 444]);
+  }, 2000);
+});
 
-    setTimeout(id => {
+const getRecipe = recID => {
+  return new Promise((resolve, reject) => {
+    setTimeout(ID => {
       const recipe = {
         title: 'Fresh tomoto pasta',
         publisher: 'Trong Hieu'
       };
-      console.log(`${id}: ${recipe.title}.`);
-
-      setTimeout(pub => {
-        const recipe2 = {
-          title: 'Italian Pizza',
-          publisher: pub
-        }
-        console.log(`${recipe2.title} from ${recipe2.publisher}.`);
-      }, 2000, recipe.publisher);
-    }, 2000, IDs[2]);
-  }, 2000);
-
+      resolve(`${ID}: ${recipe.title}.`);
+    }, 2000, recID);
+  });
 };
-getRecipe();
+
+const getRelated = publisher => {
+  return new Promise((resolve, reject) => {
+    setTimeout(pub => {
+      const recipe2 = {
+        title: 'Italian Pizza',
+        publisher: pub
+      };
+      resolve(`${recipe2.title} from ${recipe2.publisher}.`);
+    }, 2000, publisher);
+  });
+};
+
+// Consume
+getIDs
+  .then(IDs => {
+    console.log(IDs);
+    return getRecipe(IDs[2]);
+  })
+  .then(recipe => {
+    console.log(recipe);
+    return getRelated('Trong Hieu');
+  })
+  .then(related => {
+    console.log(related);
+  })
+  .catch(error => {
+    console.log(error);
+  });
